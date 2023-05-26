cbc85d0546cda61bb360cb79dd2f9b8e4be702fb
AmazingMaurice <amazingmaurice1359@gmail.com>
2023-05-26 06:59:28 -0700

    simple_shell project by maurice and manal

--- my_pipe.c
+++ my_pipe.c
@@ -14,7 +14,6 @@ int MMnum_commands = 0;
 int MMpipefd[MAX_COMMANDS - 1][2];
 char *MMcommands[PIPELINE_MAX_COMMANDS];
 char *MMtoken;
-int MMi, MMj;
 
 MMtoken = strtok(pipeline, "|");
 while (MMtoken != NULL && MMnum_commands < PIPELINE_MAX_COMMANDS)
@@ -27,54 +26,14 @@ if (MMnum_commands < 2)
 fprintf(stderr, "Invalid pipeline command\n");
 return (1);
 }
+create_pipes(MMpipefd, MMnum_commands);
 
-for (MMi = 0; MMi < MMnum_commands - 1; MMi++)
-{
-if (pipe(MMpipefd[MMi]) == -1)
-{
-perror("pipe");
-return (1);
-}
-}
-for (MMi = 0; MMi < MMnum_commands; MMi++)
-{
-pid_t pid = fork();
-if (pid == -1)
-{
-perror("fork");
-return (1);
-}
-if (pid == 0)
-{
-if (MMi == 0)
-{
-dup2(MMpipefd[MMi][1], STDOUT_FILENO);
-}
-else if (MMi == MMnum_commands - 1)
-{
-dup2(MMpipefd[MMi - 1][0], STDIN_FILENO);
-}
-else
-{
-dup2(MMpipefd[MMi - 1][0], STDIN_FILENO);
-dup2(MMpipefd[MMi][1], STDOUT_FILENO);
-}
-for (MMj = 0; MMj < MMnum_commands - 1; MMj++)
-{
-close(MMpipefd[MMj][0]);
-close(MMpipefd[MMj][1]);
-}
-return (handle_command(MMcommands[MMi], envp));
-}
-}
-for (MMi = 0; MMi < MMnum_commands - 1; MMi++)
-{
-close(MMpipefd[MMi][0]);
-close(MMpipefd[MMi][1]);
-}
-for (MMi = 0; MMi < MMnum_commands; MMi++)
-{
-wait(NULL);
-}
+execute_commands(MMcommands, pipefd, MMnum_commands, envp);
+
+close_pipes(MMpipefd, MMnum_commands);
+
+wait_for_command(MMnum_commands);
+
+
 return (0);
 }
